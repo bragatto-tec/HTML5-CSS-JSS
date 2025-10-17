50d10546ff846f864f31015f83605dd710385f71
Guilherme Miyamoto Bragatto <99208444+bragatto-tec@users.noreply.github.com>
2025-10-17 17:40:23 -0300

    Add files via upload

---
+++ App Previsão Tempo/script.js
@@ -0,0 +1,22 @@
+
+const key = "514eefcd08c5fc03a0944b2a96c89a42"
+
+function colocarNaTela(dados) {
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".icone").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+    buscarCidade(cidade)
+}
